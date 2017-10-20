fafb73dbbf0c185b74c7c40b8506e347cae1027c
Baylee Williams <bawilliams@redventures.net>
2017-10-19 23:28:18 -0400

    first commit

---
+++ C-Easy/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+
+            if (number < 10)
+            {
+                Console.WriteLine("This number is too small");
+            }
+            else if (number >= 10)
+            {
+                Console.WriteLine("This number is big enough");
+            }
+        }
+    }
+}
+
